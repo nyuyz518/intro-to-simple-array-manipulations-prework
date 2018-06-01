133495aad9e56227cbb0848dda1ee0f5228321d0
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-01 18:25:01 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push (array, string)
+  new_arr = []
+  new_arr = array.push(string)
+  new_arr
+end 
+
+def using_unshift (array, string)
+  new_arr = []
+  new_arr = array.unshift(string)
+  new_arr
+end 
+
+def using_pop (array)
+array.pop 
+array.pop
+end 
+
+def pop_with_args
+
+end 
+
+
+def using_shift (array)
+  array[0]
+  array.length = array.length -1 
+end 
+  
+def shift_with_args (array)
+  
+
